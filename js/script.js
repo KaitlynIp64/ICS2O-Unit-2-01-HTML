8308656d2d7f80bf3225e618ab3bee19353166f5
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-03-08 18:30:44 +0000

    added css and js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Kaitlyn Ip All rights reserved
+//
+// Created by: Kaitlyn Ip
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // this function displays "Hello, World!"
+
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+}
